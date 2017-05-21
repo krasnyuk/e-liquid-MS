dd5d94d8ae131c1803772af900bcc39d7b0c9a85
krasnyuk <krasnyuk.photo@gmail.com>
2017-05-21 13:52:31 +0300

    round func

--- Client/app/+pages/dashboard/dashboard.component.ts
+++ Client/app/+pages/dashboard/dashboard.component.ts
@@ -8,6 +8,55 @@ import {BaseComponent} from "../../core/base/base-component";
 })
 export class DashboardComponent extends BaseComponent {
     options: Object;
+    optionsPieChart: Object = {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Browser market shares January, 2015 to May, 2015'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Microsoft Internet Explorer',
+                y: 56.33
+            }, {
+                name: 'Chrome',
+                y: 24.03,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Firefox',
+                y: 10.38
+            }, {
+                name: 'Safari',
+                y: 4.77
+            }, {
+                name: 'Opera',
+                y: 0.91
+            }, {
+                name: 'Proprietary or Undetectable',
+                y: 0.2
+            }]
+        }]
+    };
 
     public dashboardInfo: any = {
         totalSold: 0,
@@ -26,6 +75,8 @@ export class DashboardComponent extends BaseComponent {
 
         this.dataAnalyticsService.getDashboardInfo().subscribe(success => {
             this.dashboardInfo = success;
+            this.dashboardInfo.totalProfit = Math.floor(success.totalProfit);
+            this.dashboardInfo.averageOrder = Math.floor(success.averageOrder);
         });
         this.dataAnalyticsService.getOrdersChartData().subscribe((success: Array<any>) => {
             const dates: Array<string | null> = [];
